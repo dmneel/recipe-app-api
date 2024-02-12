b72e05654411163a533e8f2c418e543ffe255575
diptanil5 <dmneel5@gmail.com>
2024-02-12 19:08:11 +0530

    Added Postgresql

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+    """This function adds two numbers"""
+    return x + y
+
+def subtract(x, y):
+    """This fucntion subtracts two numbers"""
+    return abs(x-y)
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+# Sample Tests
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    # Test adding number together
+
+    def test_add_numbers(self):
+        result = calc.add(5, 6)
+
+        self.assertEqual(result, 11)
+
+    def test_subtract_numbers(self):
+        result = calc.subtract(10, 15)
+
+        self.assertEqual(result, 5)
